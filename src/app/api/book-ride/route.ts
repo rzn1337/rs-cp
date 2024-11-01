0612b017316e6fd7ddd8896940c6957ad7d0c07e
Syed Ali Rizwan <alirizwan2003@gmail.com>
2024-11-02 00:35:58 +0500

    add book-ride api endpoint

---
+++ src/app/api/book-ride/route.ts
@@ -0,0 +1,52 @@
+import { getTokenData } from "@/app/helpers/getTokenData";
+import prisma from "@/app/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+    try {
+        const body = await request.json();
+        const { rideID } = body;
+
+        const userID = (await getTokenData(request)) as string;
+
+        const existingBooking = await prisma.passengerOnRide.findUnique({
+            where: {
+                userID_rideID: { userID, rideID },
+            },
+        });
+
+        if (existingBooking) {
+            return NextResponse.json(
+                {
+                    message: "Booking already exists",
+                    success: false,
+                },
+                { status: 409 }
+            );
+        }
+
+        const booking = await prisma.passengerOnRide.create({
+            data: { rideID, userID },
+        });
+
+        if (!booking) {
+            return NextResponse.json(
+                {
+                    message: "Failed to create ride booking",
+                    success: false,
+                },
+                { status: 500 }
+            );
+        }
+
+        const response = NextResponse.json({
+            message: "Ride booked successfully",
+            data: booking,
+            success: true,
+        });
+
+        return response;
+    } catch (error) {
+        return Response.json({ error: error.message }, { status: 500 });
+    }
+}
