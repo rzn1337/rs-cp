b691ef55eacba96faa79648ee7872479b245d60b
Syed Ali Rizwan <alirizwan2003@gmail.com>
2024-11-09 02:22:11 +0500

    fix ride display at the dashboard

--- src/app/api/get-public-rides/route.ts
+++ src/app/api/get-public-rides/route.ts
@@ -4,62 +4,91 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     try {
-        const params = request.nextUrl.searchParams;
-        const searchTerm = params.get("search")!;
-        const limit = params.get("limit")!;
+        //     const params = request.nextUrl.searchParams;
+        //     const searchTerm = params.get("search")!;
+        //     const limit = params.get("limit")!;
 
         const userID = (await getTokenData(request)) as string;
 
-        const where = searchTerm
-            ? {
-                  OR: [
-                      { to: { contains: searchTerm, mode: "insensitive" } },
-                      { from: { contains: searchTerm, mode: "insensitive" } },
-                  ],
-                  ride: {
-                      status: "SCHEDULED",
-                      driverID: { not: userID },
-                      passengers: {
-                          none: {
-                              userID: userID, // Exclude rides the user has already booked
-                          },
-                      },
-                  },
-              }
-            : {
-                  ride: {
-                      status: "SCHEDULED",
-                      driverID: { not: userID },
-                      passengers: {
-                          none: {
-                              userID: userID, // Exclude rides the user has already booked
-                          },
-                      },
-                  },
-              };
+        //     const where = searchTerm
+        //         ? {
+        //               OR: [
+        //                   { to: { contains: searchTerm, mode: "insensitive" } },
+        //                   { from: { contains: searchTerm, mode: "insensitive" } },
+        //               ],
+        //               ride: {
+        //                   status: "SCHEDULED",
+        //                   driverID: { not: userID },
+        //                   passengers: {
+        //                       none: {
+        //                           userID: userID, // Exclude rides the user has already booked
+        //                       },
+        //                   },
+        //               },
+        //           }
+        //         : {
+        //               ride: {
+        //                   status: "SCHEDULED",
+        //                   driverID: { not: userID },
+        //                   passengers: {
+        //                       none: {
+        //                           userID: userID, // Exclude rides the user has already booked
+        //                       },
+        //                   },
+        //               },
+        //           };
 
-        const rides = await prisma.route.findMany({
-            where,
-            take: parseInt(limit, 10),
-            include: {
-                ride: {
-                    include: {
-                        driver: true,
-                        vehicle: {
-                            include: { seats: true },
-                        },
-                        passengers: true,
+        //     const rides = await prisma.route.findMany({
+        //         where,
+        //         take: parseInt(limit, 10),
+        //         include: {
+        //             ride: {
+        //                 include: {
+        //                     driver: true,
+        //                     vehicle: {
+        //                         include: { seats: true },
+        //                     },
+        //                     passengers: true,
+        //                 },
+        //             },
+        //         },
+        //         orderBy: {
+        //             ride: {
+        //                 createdAt: "desc",
+        //             },
+        //         },
+        //     });
+
+        const rides = await prisma.ride.findMany({
+            where: { driverID: { not: userID } },
+            select: {
+                id: true,
+                driver: {
+                    select: {
+                        id: true,
+                        username: true,
                     },
                 },
-            },
-            orderBy: {
-                ride: {
-                    createdAt: "desc",
+                vehicle: {
+                    select: {
+                        id: true,
+                        make: true,
+                        model: true,
+                        year: true,
+                        type: true,
+                    },
                 },
+                fare: true,
+                premiumFare: true,
+                scheduledFor: true,
+                passengers: true,
+                route: { select: { id: true, to: true, from: true } },
+            },
+            take: 10,
+            orderBy: {
+                createdAt: "desc",
             },
         });
-
-        console.log(rides);
 
         const response = NextResponse.json({
             message: "Created rides fetched successfully",
